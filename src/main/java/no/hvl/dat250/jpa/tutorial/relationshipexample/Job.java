a8df45286451ac95ba8210b167e3c9f20e5ce3a8
Eivind Sulen <eivindsulen2002@gmail.com>
2024-09-18 11:50:36 +0200

    Completed tutorial
    Added the entities to persistence.xml

---
+++ src/main/java/no/hvl/dat250/jpa/tutorial/relationshipexample/Job.java
@@ -0,0 +1,35 @@
+package no.hvl.dat250.jpa.tutorial.relationshipexample;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double salary;
+    private String jobDescription;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getJobDescription() {
+		return jobDescription;
+	}
+	public void setJobDescription(String jobDescription) {
+		this.jobDescription = jobDescription;
+	}
+    
+}
